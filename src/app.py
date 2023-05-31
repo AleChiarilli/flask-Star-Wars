877423fc99c2c33caea5b0b52c3afaf035154068
Alejandro Chiarilli <125934002+AleChiarilli@users.noreply.github.com>
2023-05-31 18:28:32 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -47,8 +47,8 @@ def sitemap():
 #     return jsonify(response_body), 200
 
 
-        # éstos son mis endpoints
-        # 1. éste es el endpoint para recibir la info de todos los usuarios
+    # éstos son mis endpoints
+    # 1. éste es el endpoint para recibir la info de todos los usuarios
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -63,23 +63,17 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-         #2. éste es el endpoint para consultar UN dato o usuario en la tabla
+         #2. éste es el endpoint para consultar UN dato o usuario en la tabla, éste método puede devolver varios elementos con el
+         #   mismo ID, si no se usa la función first()
 @app.route('/user/<int:id>', methods=['GET'])
 def get_user(id):
     print(id)
 
     user = User.query.filter_by(id=id).first()
-    print(user.serialize())
-    # results = User.query.all()
-    # users_list = list(map(lambda item: item.serialize(),results))
+    data = user.serialize()
+    
 
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response ",
-        "result": user.serialize()
-    }
-
-    return jsonify(response_body), 200
+    return jsonify(data), 200
 
 
          #3. éste es el endpoint para crear un dato o usuario en la tabla
@@ -87,8 +81,13 @@ def get_user(id):
 def create_user():
 
     body = json.loads(request.data)
-    # json.loads(request.body.decode(encoding='UTF-8'))
-    print(body)
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'password' not in body:
+        raise APIException('You need to specify the password', status_code=400)
+    if 'email' not in body:
+        raise APIException('You need to specify the email', status_code=400)
     # esta linea busca el is active y lo pondra por defecto en True
     is_active = request.json.get("is_active", True)
     user = User(email=body["email"], password=body["password"], is_active=is_active)
@@ -97,7 +96,7 @@ def create_user():
     db.session.commit()
 
     response_body = {
-        "msg": "El usuario ha sido creado",
+        "msg": "The user has been created",
     }
 
     return jsonify(response_body), 200
@@ -120,6 +119,117 @@ def delete_user(id):
     return jsonify(response_body), 200
 
 
+# estos son los endpoints para los planetas
+# ############################################
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+
+    results = Planets.query.all()
+    planets_list = list(map(lambda item: item.serialize(),results))
+
+    return jsonify(planets_list), 200
+
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_planet(id):
+    print(id)
+    # éste es el pedido de un solo planeta, pero con el método get
+    planet = Planets.query.get(id)
+    data = planet.serialize()
+
+    return jsonify(data), 200
+
+@app.route('/planets', methods=['POST'])
+def create_planet():
+
+    body = json.loads(request.data)
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'name' not in body:
+        raise APIException('You need to specify the name', status_code=400)
+    if 'size' not in body:
+        raise APIException('You need to specify the size', status_code=400)
+    # esta linea busca el is active y lo pondra por defecto en True
+    planet = Planets(name=body["name"], size=body["size"])
+    # session es una palabra reservada de SQL-Alchemy
+    db.session.add(planet)
+    db.session.commit()
+
+    response_body = {
+        "msg": "El planeta ha sido creado",
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route('/api/planets/<int:id>', methods=['DELETE'])
+def delete_planet(id):
+    print(id)
+
+    # el filter_by te identifica el usuario
+    planet = Planets.query.filter_by(id=id).first()
+    print(planet.serialize())
+    # session es una palabra reservada de SQL-Alchemy
+    db.session.delete(planet)
+    db.session.commit()
+
+    response_body = {
+        "msg": "El planeta ha sido borrado",
+    }
+    return jsonify(response_body), 200
+
+# AQUÍ VIENEN LOS ENDPOINTS PARA LOS CHARACTERS
+# ############################################
+@app.route('/characters', methods=['GET'])
+def handle_characters():
+    results = Characters.query.all()
+    characters_list = list(map(lambda item: item.serialize(),results))
+    return jsonify(characters_list), 200
+
+
+@app.route('/characters/<int:id>', methods=['GET'])
+def get_character(id):
+    print(id)
+    # éste es el pedido de un solo planeta, pero con el método get
+    character = Characters.query.get(id)
+    data = character.serialize()
+    return jsonify(data), 200
+
+
+@app.route('/characters', methods=['POST'])
+def create_character():
+    body = json.loads(request.data)
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'name' not in body:
+        raise APIException('You need to specify the name', status_code=400)
+    if 'gender' not in body:
+        raise APIException('You need to specify the gender', status_code=400)
+    # esta linea busca el is active y lo pondra por defecto en True
+    character = Characters(name=body["name"], gender=body["gender"])
+    # session es una palabra reservada de SQL-Alchemy
+    db.session.add(character)
+    db.session.commit()
+    response_body = {
+        "msg": "the character has been created",
+    }
+    return jsonify(response_body), 200
+
+
+@app.route('/api/characters/<int:id>', methods=['DELETE'])
+def delete_character(id):
+    print(id)
+    # el filter_by te identifica el usuario
+    character = Characters.query.filter_by(id=id).first()
+    print(character.serialize())
+    # session es una palabra reservada de SQL-Alchemy
+    db.session.delete(character)
+    db.session.commit()
+    response_body = {
+        "msg": "The character has been deleted",
+    }
+    return jsonify(response_body), 200
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,8 +7,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    favorite_planets = db.relationship('FavoritePlanets', backref='user', lazy=True)
-    favorite_characters = db.relationship('FavoriteCharacters', backref='user', lazy=True)
+    favorite_planets = db.relationship('FavoritePlanets', backref='user', lazy=True, cascade="all, delete-orphan")
+    favorite_characters = db.relationship('FavoriteCharacters', backref='user', lazy=True, cascade="all, delete-orphan")
               # clase a la que haces referencia , 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -20,11 +20,14 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
+
+
+
 class Planets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     size = db.Column(db.Integer, unique=False, nullable=False)
-    favorite_planets = db.relationship('FavoritePlanets', backref='planets', lazy=True)
+    favorite_planets = db.relationship('FavoritePlanets', backref='planets', lazy=True, cascade="all, delete-orphan")
 
     def __repr__(self):
         return '<Planets %r>' % self.name
@@ -40,7 +43,7 @@ class Characters(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     gender = db.Column(db.String, unique=False, nullable=False)
-    favorite_characters = db.relationship('FavoriteCharacters', backref='characters', lazy=True)
+    favorite_characters = db.relationship('FavoriteCharacters', backref='characters', lazy=True, cascade="all, delete-orphan")
 
 
     def __repr__(self):
@@ -83,3 +86,53 @@ class FavoriteCharacters(db.Model):
             "name": self.user_id,
             # do not serialize the password, its a security breach
         }
+    
+
+
+    # modelo de tablas con tablas relacionales
+
+# meal_ingredient = db.Table("meal_ingredient",
+#                            db.Column("ingredient_id", db.Integer, db.ForeignKey(
+#                                "ingredients.id"), primary_key=True),
+#                            db.Column("meal_id", db.Integer, db.ForeignKey(
+#                                "meals.id"),  primary_key=True),
+#                            )
+
+
+# class Meals(db.Model):
+#     __tablename__ = "meals"
+#     id = db.Column(db.Integer, primary_key=True)
+#     meal = db.Column(db.String(100), nullable=False)
+#     ingredients = db.relationship("Ingredients", secondary=meal_ingredient,
+#                                   lazy="subquery", backref=db.backref("meal_ingredients"))
+#     price = db.Column(db.Integer, nullable=False)
+
+#     def __repr__(self):
+#         return f"{self.meal}"
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "meal": self.meal,
+#             "ingredients": self.ingredients,
+#             "price": self.price
+#         }
+
+
+# class Ingredients(db.Model):
+#     __tablename__ = "ingredients"
+#     id = db.Column(db.Integer, primary_key=True)
+#     ingredient = db.Column(db.String(100), nullable=False)
+#     cuantity = db.Column(db.Integer, nullable=False)
+#     availeable = db.Column(db.Boolean, default=True, nullable=False)
+
+#     def __repr__(self):
+#         return f"{self.ingredient}"
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "ingredient": self.ingredient,
+#             "cuantity": self.cuantity,
+#             "availeable": self.availeable
+#         }
